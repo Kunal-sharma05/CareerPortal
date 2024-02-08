98c0351ec34c3eddc727a9272037c3feda535bca
kunal-sharma05 <cse.190320027@silicon.ac.in>
2024-02-08 20:46:10 +0530

    Initial repository

---
+++ src/main/java/com/hexa/CareerPortal/CareerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.hexa.CareerPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexa/CareerPortal/CareerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexa.CareerPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
