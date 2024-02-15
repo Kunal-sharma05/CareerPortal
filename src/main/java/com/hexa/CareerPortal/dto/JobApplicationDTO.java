66a3c180b2ad2eb3e83ae0389256198dcf7f985c
asimzohairyazdani <asimzohair4@gmail.com>
2024-02-16 00:30:10 +0530

    committing

---
+++ src/main/java/com/hexa/CareerPortal/dto/JobApplicationDTO.java
@@ -0,0 +1,40 @@
+package com.hexa.CareerPortal.dto;
+
+import com.hexa.CareerPortal.entity.Status;
+import java.time.LocalDateTime;
+
+public class JobApplicationDTO {
+ 
+    private Status status;
+    private LocalDateTime applicationUpdatedDate;
+    
+	public JobApplicationDTO(Status status, LocalDateTime applicationUpdatedDate) {
+		super();
+		this.status = status;
+		
+		this.applicationUpdatedDate = applicationUpdatedDate;
+		
+	}
+	public JobApplicationDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public LocalDateTime getApplicationUpdatedDate() {
+		return applicationUpdatedDate;
+	}
+	public void setApplicationUpdatedDate(LocalDateTime applicationUpdatedDate) {
+		this.applicationUpdatedDate = applicationUpdatedDate;
+	}
+	@Override
+	public String toString() {
+		return "JobApplicationDTO [status=" + status + ", applicationDate="
+				+ ", applicationUpdatedDate=" + applicationUpdatedDate + "]";
+	}
+    
+}
