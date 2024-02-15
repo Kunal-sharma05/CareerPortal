ac707b515428e6b66ac531db288b7e6ea23c2b4b
kunal-sharma05 <cse.190320027@silicon.ac.in>
2024-02-15 18:44:35 +0530

    Addded dependency of hateos And Made some modification of exception classes

--- src/main/java/com/hexa/CareerPortal/exception/ErrorDetails.java
+++ src/main/java/com/hexa/CareerPortal/exception/ErrorDetails.java
@@ -3,6 +3,12 @@ package com.hexa.CareerPortal.exception;
 import java.time.LocalDateTime;
 
 public class ErrorDetails {
+	public ErrorDetails(LocalDateTime timeStamp, String message, String errorCode) {
+		super();
+		this.timeStamp = timeStamp;
+		this.message = message;
+		this.errorCode = errorCode;
+	}
 	private LocalDateTime timeStamp;
 	private String message;
 	private String path;
@@ -14,32 +20,21 @@ public class ErrorDetails {
 		this.path = path;
 		this.errorCode = errorCode;
 	}
-	public ErrorDetails() {
-		super();
-	}
+	
 	public LocalDateTime getTimeStamp() {
 		return timeStamp;
 	}
-	public void setT(LocalDateTime timeStamp) {
-		this.timeStamp = timeStamp;
-	}
+
 	public String getMessage() {
 		return message;
 	}
-	public void setM(String message) {
-		this.message = message;
-	}
+
 	public String getPath() {
 		return path;
 	}
-	public void setPath(String path) {
-		this.path = path;
-	}
+
 	public String getErrorCode() {
 		return errorCode;
-	}
-	public void setErrorCode(String errorCode) {
-		this.errorCode = errorCode;
 	}
 	@Override
 	public String toString() {
