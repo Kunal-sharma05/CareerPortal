bfbca7649d082eeec2f0f25fcd4bc8a2afa602d7
asimzohairyazdani <asimzohair4@gmail.com>
2024-03-17 13:55:12 +0530

    committing

---
+++ frontend/src/components/CareerBlogs.jsx
@@ -0,0 +1,172 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { SideNav } from "./templates/SideNav";
+import { useNavigate, Link } from "react-router-dom";
+import Accenture from "./Assets/Accenture.png"
+export function  CareerBlogs() {
+  const navigate = useNavigate();
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  };
+  const onReadMoreHandler = () => {
+ 
+  };
+  return (
+    <div className="w-full h-full flex flex-col overflow-x-hidden">
+      <Header />
+      <div className="bg-gradient-to-r from-slate-900 via-red-900 w-[73%] h-full flex gap-4">
+        <SideNav />  
+        
+    <div className='w-3/4 m-auto pl-40'>
+        
+      <div className="mt-15 w-[130%] ">
+        
+      <Slider {...settings}>
+        {data.map((d) => (
+          <div key={d.name} className="bg-white h-[450px] text-black rounded-xl">
+            <div className='h-56 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black flex justify-center items-center rounded-t-xl'>
+              <img src={d.imgUrl} alt="" className="h-44 w-44 rounded-full"/>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-6 p-1">
+              <p className="text-xl font-semibold">{d.name}</p>
+              <p className="text-center">{d.review}</p>
+              <button onClick={onReadMoreHandler} className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black text-white text-lg px-8 py-1 rounded-xl'><a href={d.blog} >Read More </a></button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      </div>
+      </div>
+      </div>
+     
+      <Footer />
+    </div>
+  );
+}
+
+const data = [
+  {
+    name: `Mckinsey`,
+    imgUrl: `https://images.app.goo.gl/QtoYqEryA1ADimHbA`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    blog: `https://www.mckinsey.com/careers/meet-our-people/careers-blog`
+  },
+  {
+    name: `MentorCruise on Tesla`,
+    imgUrl: `https://images.app.goo.gl/6eytzniGisFuC3m96`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    blog: `https://mentorcruise.com/blog/tesla-recruitment-and-selection-process-how-to-get-hired-at-tesla/`
+  },
+  {
+    name: `Microsoft `,
+    imgUrl: `https://images.app.goo.gl/C6eoqxJcY6QYvguAA `,
+    review:` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    blog : `https://news.microsoft.com/life/spending-time-behind-bars-employee-set-no-bars-success/`
+  },
+  {
+    name:`Accenture`,
+    imgUrl: `https://images.app.goo.gl/ekgMC13UrhJoQkq16`,
+    review:`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    blog: `https://www.accenture.com/us-en/blogs/blogs-careers`
+  },
+  {
+    name: `Hexaware Technologies`,
+    imgUrl: `https://images.app.goo.gl/44LGBVJXQCC6CYDaA`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    blog: `https://hexaware.com/blogs/recruitment-the-millennials-way/`
+  },
+  
+];
+
+export default CareerBlogs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/free-mode";
+
+import { FreeMode, Pagination } from "swiper/modules";
+
+import { RxArrowTopRight } from "react-icons/rx";
+import { ServiceData } from "../constants";
+
+export const CareerBlogs = () => {
+  return (
+    <div className="flex items-center justify-center flex-col h-[900px] bg-[#6c34af]">
+      <Swiper
+        breakpoints={{
+          340: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+          },
+          700: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+          },
+        }}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="max-w-[90%] lg:max-w-[80%]"
+      >
+        {ServiceData.map((item) => (
+          <SwiperSlide key={item.title}>
+            <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
+              <div
+                className="absolute inset-0 bg-cover bg-center"
+                style={{ backgroundImage: `url(${item.backgroundImage})` }}
+              />
+              <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
+              <div className="relative flex flex-col gap-3">
+                <item.icon className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" />
+                <h1 className="text-xl lg:text-2xl">{item.title} </h1>
+                <p className="lg:text-[18px]">{item.content} </p>
+              </div>
+              <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default CareerBlogs 
+*/
---
+++ frontend/src/components/JobFairs.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useMemo } from "react";
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+} from 'reactstrap';
+import { SideNav } from "./templates/SideNav";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+
+
+export function JobFairs() {
+  /*  
+  const [open, setOpen] = useState('1');
+  const toggle = (id) => {
+    if (open === id) {
+      setOpen();
+    } else {
+      setOpen(id);
+    }
+  };*/
+
+  return (
+
+    <div className="w-[100%] h-full flex flex-col overflow-x-hidden">
+    <Header />
+    <div className="bg-gradient-to-r from-slate-900 via-red-900 to-slate-900 w-full h-full flex gap-5 ">
+      <SideNav />
+      <div className="w-[80%] h-[88%]">
+      <div className="container">
+      <h1 className='text-zinc-100'>Explore Exciting Opportunities: Job Fairs Galore!</h1>
+      
+      <div className="job-fair text-zinc-100 ">
+        <h3>Tech Connect Expo</h3>
+        <p>Gear up for the ultimate tech rendezvous at the Tech Connect Expo! Hosted in the heart of downtown, this event brings together leading tech companies from Silicon Valley giants to innovative startups. Explore cutting-edge technologies, engage in insightful workshops, and network with industry pioneers. Whether you're passionate about coding, data science, or cybersecurity, the Tech Connect Expo is your gateway to a dynamic career in the tech sphere.</p>
+      </div>
+      <br></br>
+      <div className="card pl-3">
+        <h2>Google Job Fair at KIIT University</h2>
+        <p><strong>Date:</strong> April 10, 2024</p>
+        <p><strong>Time:</strong> 9:00 AM - 5:00 PM</p>
+        <p><strong>Location:</strong> KIIT University, Bhubaneswar, Odisha</p>
+        <p><strong>About the Event:</strong> Don't miss out on the exclusive opportunity to meet representatives from Google at our upcoming job fair! Whether you're an aspiring software engineer, product manager, or marketing enthusiast, Google offers a plethora of exciting career paths to explore. Engage in one-on-one discussions, participate in interactive demos, and gain valuable insights into life at Google. Elevate your career journey and embark on a transformative adventure with one of the world's most innovative companies. Mark your calendars and join us for a day of inspiration and opportunity!</p>
+      </div>
+    </div>
+    </div>
+    </div>
+    <Footer />
+    </div>
+  );
+}
+
+export default JobFairs;
