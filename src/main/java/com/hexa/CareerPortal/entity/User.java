e15440920f81c7a1db2511ad2389722e0ada5610
kunal-sharma05 <cse.190320027@silicon.ac.in>
2024-03-22 02:44:17 +0530

    corrected

--- src/main/java/com/hexa/CareerPortal/entity/User.java
+++ src/main/java/com/hexa/CareerPortal/entity/User.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name="user_deatils")
